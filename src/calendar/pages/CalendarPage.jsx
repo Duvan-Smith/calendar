9eaf8a50c448dd440439871df395eef3039fd9c6
DsmithMr <dsmith.mr@gmail.com>
2022-07-31 13:43:34 -0500

    Added event control

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { addHours } from "date-fns";
 
-import { Navbar } from "../";
+import { CalendarEvent, Navbar } from "../";
 import { getMessagesES, localizer } from "../../helpers";
 
 const events = [
@@ -48,6 +48,9 @@ export const CalendarPage = () => {
         style={{ height: "calc(100vh-80px)" }}
         messages={getMessagesES()}
         eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent,
+        }}
       />
     </>
   );
